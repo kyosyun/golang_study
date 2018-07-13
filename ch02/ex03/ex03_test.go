0404a74c5e5f3f21457f9eb684cd47d94f6e0cfa
kyo_s <kyosyunn@yahoo.co.jp>
2018-07-13 15:04:08 +0900

    fix[ch02-ex03] fix testcode

--- ch02/ex03/ex03_test.go
+++ ch02/ex03/ex03_test.go
@@ -3,22 +3,18 @@ package ex03_test
 import (
 "testing"
 "golang_study/ch02/ex03"
+	"golang_study/ch02/ex04"
+	"golang_study/ch02/popcount"
 )
 
 
 func TestPopcount(t *testing.T) {
-	i := ex03.PopCount(255)
-	if (i == 8) {
-		t.Log("success")
-	} else {
-		t.Error("err when 255")
-	}
-
-	i = ex03.PopCount(3)
-	if (i == 2) {
-		t.Log("success")
-	} else {
-		t.Error("err when 255")
+	for i := 0; i < 256 ; i++  {
+		if(ex03.PopCount(uint64(i)) == popcount.PopCount(uint64(i))) {
+			t.Log("success: ", i)
+		} else {
+			t.Error("fail: when:", i, "i: ", ex04.PopCount(uint64(i)), "expected: ", ex03.PopCount(uint64(i)))
+		}
 	}
 }
 
