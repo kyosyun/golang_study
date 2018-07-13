7733b7bbe8429fa04618c73d28c0a8687c5b62b0
kyo_s <kyosyunn@yahoo.co.jp>
2018-07-13 15:16:02 +0900

    fix test code

--- ch02/ex05/ex05_test.go
+++ ch02/ex05/ex05_test.go
@@ -1,15 +1,15 @@
 package ex05_test
 
 import (
-"testing"
-"golang_study/ch02/ex05"
 	"golang_study/ch02/ex03"
 	"golang_study/ch02/ex04"
+	"golang_study/ch02/ex05"
+	"testing"
 )
 
 func TestPopcount(t *testing.T) {
-	for i := 0; i < 256 ; i++  {
-		if(ex05.PopCount(uint64(i)) == ex04.PopCount(uint64(i))) {
+	for i := 0; i < 256; i++ {
+		if ex05.PopCount(uint64(i)) == ex04.PopCount(uint64(i)) {
 			t.Log("success: ", i)
 		} else {
 			t.Error("fail: when:", i, "i: ", ex05.PopCount(uint64(i)), "expected: ", ex04.PopCount(uint64(i)))
@@ -18,19 +18,20 @@ func TestPopcount(t *testing.T) {
 }
 
 func BenchmarkPopCountEx03(b *testing.B) {
-	for i := 0; i < b.N ; i++  {
-		ex03.PopCount(255)
+	for i := 0; i < b.N; i++ {
+		// ロジックによって0や1の処理のスピードが偏るので、PopCountの引数をばらつかせる
+		ex03.PopCount(uint64(b.N % 255))
 	}
 }
 
 func BenchmarkPopCountEx04(b *testing.B) {
-	for i := 0; i < b.N ; i++  {
-		ex04.PopCount(255)
+	for i := 0; i < b.N; i++ {
+		ex04.PopCount(uint64(b.N % 255))
 	}
 }
 
-func BenchmarkPopCountEx05(b *testing.B) {
-	for i := 0; i < b.N ; i++  {
-		ex05.PopCount(255)
+func BenchmarkPopCountEx05_(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ex05.PopCount(uint64(b.N % 255))
 	}
 }
