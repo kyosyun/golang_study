69c095b48df70ecf780d242b523c0e11a55324b1
kyo_s <kyosyunn@yahoo.co.jp>
2018-08-17 14:10:44 +0900

    add test code eof ch03

--- ch03/ex10/comma_test.go
+++ ch03/ex10/comma_test.go
@@ -3,6 +3,7 @@ package comma_test
 import (
 	"testing"
 	"golang_study/ch03/ex10"
+	"fmt"
 )
 
 func TestComma(t *testing.T) {
@@ -11,4 +12,20 @@ func TestComma(t *testing.T) {
 
 func TestCommaWithBuffer(t *testing.T) {
 	print(comma.CommaWithBuffer("1235") + "\n")
+
+	for _, test := range []struct{
+		data string
+		expected string
+	}{
+		{"", ""},
+		{"123","123"},
+		{"123","123"},
+		{"123456","123,456"},
+		{"123456789","123,456,789"},
+	}{
+		result := comma.Comma(test.data)
+		if result != test.expected {
+			fmt.Printf("expected: %s, result: %s",test.expected, result)
+		}
+	}
 }
--- ch03/ex11/comma_test.go
+++ ch03/ex11/comma_test.go
@@ -13,3 +13,35 @@ func TestComma(t *testing.T) {
 	print(comma.CommaWithSinged("+12351234.5") + "\n")
 	print(comma.CommaWithSinged("-12351234.5") + "\n")
 }
+
+func TestCommaWithSinged(t *testing.T) {
+	for _, test := range []struct {
+		data string
+		expected string
+	}{
+		//{"",""},
+		{"123","123"},
+		{"1234","1234"},
+		{"1.234","1.234"},
+		{"12.34","12.34"},
+		{"123.4","123.4"},
+
+		{"-123","-123"},
+		{"-1234","-1234"},
+		{"-1.234","-1.234"},
+		{"-12.34","-12.34"},
+		{"-123.4","-123.4"},
+
+		{"+123","+123"},
+		{"+1234","+1234"},
+		{"+1.234","+1.234"},
+		{"+12.34","+12.34"},
+		{"+123.4","+123.4"},
+
+	} {
+		result := comma.CommaWithSinged(test.data)
+		if result != test.expected {
+			t.Errorf("result = %q, but want %q", result, test.expected)
+		}
+	}
+}
