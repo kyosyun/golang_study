a01303d91f91df4d68328344114d8634e4e6b2e3
kyo_s <kyosyunn@yahoo.co.jp>
2018-08-17 14:29:55 +0900

    update test ch03 ex12

--- ch03/ex12/anagrams_test.go
+++ ch03/ex12/anagrams_test.go
@@ -10,3 +10,22 @@ func TestCheckAnagram(t *testing.T) {
 	println(anagrams.CheckAnagram("abc", "cba"))
 	println(anagrams.CheckAnagram("abc", "abd"))
 }
+
+func TestCheckAnagram2(t *testing.T) {
+	for _, test := range []struct{
+		a, b string
+		exected bool
+	}{
+		{"a","a",true},
+		{"a","b",false},
+		{"日本","日本",true},
+		{"日本","本日",true},
+		{"日本日本","日本",false},
+		{"日a本","日b本",false},
+	}{
+		result := anagrams.CheckAnagram(test.a, test.b)
+		if result != test.exected {
+			t.Errorf("result = %v, but was = %vk", result, test.exected)
+		}
+	}
+}
